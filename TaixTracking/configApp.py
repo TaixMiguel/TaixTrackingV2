f1d19dbad5d988a20af72c1f9771a50b682807f3
Miguelín <unbikercualquiera@gmail.com>
2023-01-22 17:52:23 +0100

    [ADDED] Se añade al cron la eliminación de usuarios no activos

--- TaixTracking/configApp.py
+++ TaixTracking/configApp.py
@@ -1,3 +1,6 @@
+import json
+import logging
+import os
 from threading import Lock
 
 
@@ -16,16 +19,39 @@ class ConfigAppMeta(type):
 
 class ConfigApp(metaclass=ConfigAppMeta):
 
+    __configData: dict = {}
     __sw_daemon_launch: bool = False
 
     def __init__(self) -> None:
-        pass
-
-    def get_interval_time_search_tracking(self) -> int:
-        return 60
+        path_file: str
+        try:
+            from TaixTracking import settings
+            path_file = os.environ['CONFIG_FILE_TAIXTRACKING']
+            with open(path_file, 'r') as config_file:
+                self.__configData = json.load(config_file)
+        except KeyError:
+            logging.error('No se ha definido la variable de entorno CONFIG_FILE_TAIXTRACKING')
+            logging.info('Se detiene la ejecución de la aplicación')
+            quit()
+        except FileNotFoundError:
+            logging.error(f'No se encuentra el fichero de configuración "{path_file}"')
+            logging.info('Se detiene la ejecución de la aplicación')
+            quit()
 
     def is_daemon_launch(self) -> bool:
         return self.__sw_daemon_launch
 
     def set_daemon_launch(self, daemon_launch: bool) -> None:
         self.__sw_daemon_launch = daemon_launch
+
+    def get_number_days_to_delete_user_inactive(self) -> int:
+        try:
+            return self.__configData['application']['days_to_delete_inactive_users']
+        except KeyError:
+            return 5
+
+    def get_interval_time_search_tracking(self) -> int:
+        try:
+            return self.__configData['application']['interval_time_search_tracking']
+        except KeyError:
+            return 60
--- daemon/daemonWatcher.py
+++ daemon/daemonWatcher.py
@@ -1,7 +1,19 @@
+import logging
 import threading
 import time
 
 from TaixTracking.configApp import ConfigApp
+
+
+def _delete_inactive_users(num_days: int) -> None:
+    from tracking.models import User
+    from datetime import date, timedelta
+
+    aux_date = date.today() - timedelta(days=num_days)
+    users_to_remove = User.objects.filter(sw_allow=False, creation_time__lte=aux_date)
+    if users_to_remove:
+        logging.info(f'{len(users_to_remove)} usuarios a eliminar a fecha {aux_date.strftime("%m/%d/%y")}')
+        users_to_remove.delete()
 
 
 class DaemonWatcher:
@@ -20,6 +32,8 @@ class DaemonWatcher:
     def __turn_on(self) -> None:
         self.turnOff = False
         ConfigApp().set_daemon_launch(True)
+
+        _delete_inactive_users(ConfigApp().get_number_days_to_delete_user_inactive())
 
         while not self.turnOff:
             time.sleep(self.timeSleep)
