3a5ca17aa3335997e0a2735ddf97c8d7860641a6
Miguelín <unbikercualquiera@gmail.com>
2023-02-02 20:59:00 +0100

    [ADDED] Se añade la posibilidad de compartir tracking entre usuarios

--- communication/telegram.py
+++ communication/telegram.py
@@ -8,7 +8,7 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 
 from TaixTracking import kTaixTracking
 from TaixTracking.configApp import ConfigApp
-from tracking.models import Tracking, UserAttribute
+from tracking.models import Tracking, UserAttribute, TrackingUser
 
 logger = logging.getLogger(__name__)
 
@@ -59,21 +59,30 @@ def command_aliexpress(update: Update, context: CallbackContext) -> None:
         return
 
     tracking: Tracking = None
-    try:
-        tracking = Tracking(track_type=kTaixTracking.Tracking.Types.CAINIAO, track_code=track_order,
-                            id_creator_user_fK=user)
-        tracking.save()
-    except IntegrityError as e:
-        logger.info(f'Se ha intentado dar de alta nuevamente el track "{kTaixTracking.Tracking.Types.CAINIAO}#'
-                    f'{track_order}"')
-        trackings = Tracking.objects.filter(track_type=kTaixTracking.Tracking.Types.CAINIAO)\
-                                    .filter(track_code=track_order)
-        if trackings:
-            tracking = trackings[0]
+    trackings = Tracking.objects.filter(track_type=kTaixTracking.Tracking.Types.CAINIAO).filter(track_code=track_order)
+    if trackings:
+        tracking_repeat = True
+        tracking = trackings[0]
+    else:
+        try:
+            tracking = Tracking(track_type=kTaixTracking.Tracking.Types.CAINIAO, track_code=track_order,
+                                id_creator_user_fK=user)
+            tracking.save()
+        except IntegrityError as e:
+            logger.info(f'Se ha producido un error al dar de alta el track "{kTaixTracking.Tracking.Types.CAINIAO}#'
+                        f'{track_order}"')
+            logger.exception(e)
 
-    # TODO: asociar el tracking al usuario
-    if user.is_active:
-        update.message.reply_text('Es posible que tardes un rato en recibir una respuesta. Por favor, sé paciente.')
+    track_user: TrackingUser = TrackingUser.objects.filter(id_tracking_fk=tracking).filter(id_user_fK=user)
+    if not track_user:
+        TrackingUser(id_tracking_fk=tracking, id_user_fK=user).save()
+        if user.is_active:
+            update.message.reply_text('Es posible que tardes un rato en recibir una respuesta. Por favor, sé paciente.')
+    else:
+        logger.info(f'El usuario {user.username} ha intentado dar de alta nuevamente el track: '
+                    f'"{kTaixTracking.Tracking.Types.CAINIAO}#{track_order}"')
+        if user.is_active:
+            update.message.reply_text('El seguimiento que has introducido ya existe entre tus tracking.')
 
 
 class Telegram:
--- tracking/admin.py
+++ tracking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserAttribute, Tracking, TrackingDetail
+from .models import UserAttribute, Tracking, TrackingDetail, TrackingUser
 
 
 @admin.register(UserAttribute)
@@ -13,14 +13,25 @@ class TrackingDetailInline(admin.TabularInline):
     extra = 0
 
 
+class TrackingUserInline(admin.TabularInline):
+    model = TrackingUser
+    extra = 0
+
+
 @admin.register(Tracking)
 class TrackingAdmin(admin.ModelAdmin):
     list_display = ('track_type', 'track_code', 'creation_time')
     list_filter = ('track_type', 'creation_time')
-    inlines = [TrackingDetailInline]
+    inlines = [TrackingDetailInline, TrackingUserInline]
 
 
 @admin.register(TrackingDetail)
 class TrackingDetailAdmin(admin.ModelAdmin):
     list_display = ('detail_head', 'detail_text', 'audit_time')
     list_filter = ('id_tracking_fk', 'detail_head')
+
+
+@admin.register(TrackingUser)
+class TrackingUserAdmin(admin.ModelAdmin):
+    list_display = ('id_user_fK', 'id_tracking_fk')
+    list_filter = ('id_user_fK', 'id_tracking_fk')
