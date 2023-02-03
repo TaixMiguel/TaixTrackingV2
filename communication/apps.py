f20c37362d23d4f8ef89809492fc43120e5fec5d
Miguelín <unbikercualquiera@gmail.com>
2023-02-03 17:22:13 +0100

    [CHANGED] Uso de Telegram si se parametriza
    Ahora Telegram solo se utiliza si se parametriza en la configuración bajo la estructura `telegram` y `load`, y puede tener un valor de true o false

--- communication/apps.py
+++ communication/apps.py
@@ -1,11 +1,14 @@
 from django.apps import AppConfig
 
+from TaixTracking.configApp import ConfigApp
+
 
 class CommunicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'communication'
 
     def ready(self):
-        # TODO: convivir con el server de django
-        from communication.telegram import Telegram
-        Telegram().run()
+        if ConfigApp().get_value_boolean('telegram', 'load', False):
+            # TODO: convivir con el server de django
+            from communication.telegram import Telegram
+            Telegram().run()
